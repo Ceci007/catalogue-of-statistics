7f45294f79c53fa9911538ad598538ed1cadb820
Ceci007 <cecibenitezca@gmail.com>
2021-02-24 18:34:00 -0300

    TSE sugestion: Remove console.log statement from filter reducer

--- src/reducers/mainFilter.js
+++ src/reducers/mainFilter.js
@@ -4,7 +4,6 @@ const mainFilter = (state = 'market_cap_desc', action) => {
   switch (action.type) {
     case FILTER_UPDATE:
       // eslint-disable-next-line
-      console.log('@mainFilter reducer, filter is : ' + action.filter)
       return action.filter;
 
     default:
